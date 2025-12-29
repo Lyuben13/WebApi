62db217be61f0854768127602b876b85d0c6da36
LyubenAndreev <liuben.Andreev@gmail.com>
2025-12-29 15:12:01 +0200

    Add project files.

---
+++ MVC.Intro/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVC.Intro.Models;
+
+namespace MVC.Intro.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public DbSet<Product> Products { get; set; } = null!;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ MVC.Intro/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using MVC.Intro.Data;
+using MVC.Intro.Services;
+using MVC.Intro.Models;
+using System.IO;
+
+namespace MVC.Intro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                var dbPath = Path.Combine(builder.Environment.ContentRootPath, "Data", "products.db");
+                Directory.CreateDirectory(Path.GetDirectoryName(dbPath)!);
+
+                var csb = new SqliteConnectionStringBuilder
+                {
+                    DataSource = dbPath
+                };
+
+                options.UseSqlite(csb.ToString());
+            });
+
+            builder.Services
+                .AddIdentity<Users, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddTransient<ProductService>();
+
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
+            }
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
+                name: "routing",
+                pattern: "{controller=Routing}/{*default}",
+                defaults: new { controller = "Routing", action = "Default" });
+
+            app.Run();
+        }
+    }
+}
